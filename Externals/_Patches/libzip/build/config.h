a919238eec87da99ed3d453f9cf4ded746821dc3
hazmiarifin <hazmiarifin@gmail.com>
2022-02-23 09:16:39 +0700

    * fix libzip build on linux

--- Externals/_Patches/libzip/build/config.h
+++ Externals/_Patches/libzip/build/config.h
@@ -1,13 +1,7 @@
-#ifndef HAD_CONFIG_H
-#define HAD_CONFIG_H
-#define HAVE_FSEEKO
-#define HAVE_MKSTEMP
-#define HAVE_MOVEFILEEXA
-#define HAVE__SNPRINTF
-#define HAVE_SNPRINTF
-#define HAVE__STRCMPI
-#define HAVE__STRICMP
-#define HAVE_UNISTD_H
-#define PACKAGE "libzip"
-#define VERSION "0.10.0"
-#endif /* HAD_CONFIG_H */
+#pragma once
+
+#if defined(WIN32)
+    #include "config_win32.h"
+#else
+    #include "config_linux.h"
+#endif
