80c21d56943de04003a5c368ebef5df82617ca4e
hazmiarifin <hazmiarifin@gmail.com>
2023-05-07 08:55:57 +0700

    * revert back to libzip 0.10 to fix extraction

---
+++ Externals/_Patches/libzip/lib/config_linux.h
@@ -0,0 +1,13 @@
+#ifndef HAD_CONFIG_H
+#define HAD_CONFIG_H
+#define HAVE_FSEEKO
+#define HAVE_MKSTEMP
+/* #undef HAVE_MOVEFILEEXA */
+/* #undef HAVE__SNPRINTF */
+#define HAVE_SNPRINTF
+/* #undef HAVE__STRCMPI */
+/* #undef HAVE__STRICMP */
+#define HAVE_UNISTD_H
+#define PACKAGE "libzip"
+#define VERSION "0.10.0"
+#endif /* HAD_CONFIG_H */
