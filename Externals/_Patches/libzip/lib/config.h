7d6fdda43254ef71e33a70347a5d2cc19f5383d9
hazmiarifin <hazmiarifin@gmail.com>
2023-06-11 11:02:04 +0700

    + add asmCrashReport.patch
    - remove unused patches

--- Externals/_Patches/libzip/lib/config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#if defined(WIN32)
-    #include "config_win32.h"
-#else
-    #include "config_linux.h"
-#endif
--- Externals/_Patches/libzip/lib/zipconf.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#if defined(WIN32)
-    #include "zipconf_win32.h"
-#else
-    #include "zipconf_linux.h"
-#endif
